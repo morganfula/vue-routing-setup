322622a91a93061b00eb42ef1e570305ffbddcae
morganfula <morgan.fula@me.com>
2021-01-29 14:03:47 +0100

    redirecting & catch all routes

--- src/components/nav/NotFound.vue
+++ src/components/nav/NotFound.vue
@@ -1,4 +1,14 @@
 <template>
-  <h2>Page not found! Maybe view our <router-link>teams</router-link></h2>
+  <h2>
+    Page not found! Maybe view our
+    <router-link to="'/teams">teams</router-link>?
+  </h2>
 </template>
 
+<style lang="scss">
+h2 {
+  display: flex;
+  justify-content: center;
+  margin-top: 200px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
+import NotFound from './components/nav/NotFound.vue';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -13,7 +14,7 @@ const router = createRouter({
     { path: '/teams', component: TeamsList },
     { path: '/users', component: UsersList },
     { path: '/teams/:teamId', component: TeamMembers, props: true },
-    { path: '/:catchAll(.*)', redirect: '/teams' }
+    { path: '/:catchAll(.*)', component: NotFound }
   ],
   linkActiveClass: 'active'
 });
