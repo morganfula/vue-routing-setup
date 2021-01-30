b0f4b69f3603502e7c4285d205833ae1789809c9
morganfula <morgan.fula@me.com>
2021-01-30 16:35:05 +0100

    organizing routes files

---
+++ src/router.js
@@ -0,0 +1,55 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import TeamsList from './pages/TeamsList.vue';
+import UsersList from './pages/UsersList.vue';
+import TeamMembers from './components/teams/TeamMembers.vue';
+import NotFound from './pages/NotFound.vue';
+import TeamsFooter from './pages/TeamsFooter.vue';
+import UsersFooter from './pages/UsersFooter.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', redirect: '/teams' },
+    {
+      name: 'teams',
+      path: '/teams',
+      meta: { needsAuth: true },
+      components: { default: TeamsList, footer: TeamsFooter },
+      children: [
+        {
+          name: 'team-member',
+          path: ':teamId',
+          component: TeamMembers,
+          props: true
+        }
+      ]
+    },
+    { path: '/users', components: { default: UsersList, footer: UsersFooter } },
+    { path: '/:catchAll(.*)', component: NotFound }
+  ],
+  linkActiveClass: 'active',
+  scrollBehavior(_, _2, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    }
+    return { left: 0, top: 0 };
+  }
+});
+
+router.beforeEach(function(to, from, next) {
+  console.log('Global beforeEach');
+  console.log(to, from);
+  if (to.meta.needsAuth) {
+    console.log('Need Auth');
+    next();
+  }
+});
+
+router.afterEach((to, from) => {
+  // sending analytics data
+  console.log('Global afterEach');
+  console.log(to, from);
+});
+
+export default router;
