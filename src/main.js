d3816a2ca3ff46fb0630795c7a54022fae583812
morganfula <morgan.fula@me.com>
2021-01-30 15:20:57 +0100

    scroll postion

--- src/main.js
+++ src/main.js
@@ -29,7 +29,13 @@ const router = createRouter({
     { path: '/users', components: { default: UsersList, footer: UsersFooter } },
     { path: '/:catchAll(.*)', component: NotFound }
   ],
-  linkActiveClass: 'active'
+  linkActiveClass: 'active',
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    }
+    return { left: 0, top: 0 };
+  }
 });
 
 const app = createApp(App);
